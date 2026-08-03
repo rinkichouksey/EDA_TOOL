efa1d4e01d00e78861393ad0f34c59cbc58f7140
Rinki Chouksey <rinkichouksey04@gmail.com>
2026-08-03 06:40:09 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import pandas 
-import streamlit as st 
-from visualization import(
+import pandas as pd
+import streamlit as st
+from visualization import (
     histogram,
     box_plot,
     density_plot,
@@ -8,16 +8,16 @@ from visualization import(
     scatter_plot,
     pair_plot,
     pie_chart,
-     count_plot,
-     line_chart,
-     bar_chart,
-     missing_bar,
-     outlier_plot,
-     correlation_heatmap
-) 
-from preprocessing import *  # * ka usse all ke liy use karte hai
-from eda import *
+    count_plot,
+    line_chart,
+    bar_chart,
+    missing_bar,
+    outlier_plot,
+    correlation_heatmap
+)
+from preprocessing import *
 from utils import *
+from eda import *
 
 st.set_page_config(
     page_title="Smart EDA Tool",
@@ -29,34 +29,34 @@ st.markdown("---")
 #sidebar
 st.sidebar.title("Navigation")
 uploaded_file = st.sidebar.file_uploader(
-    "upload CSV /Excel",
-    type=["csv","xlsx","xls"]   # Excel ka extention xlsx,  xls    full form Excel Spreadsheet 
+    "upload CSV / Excel",
+    type= ["csv","xlsx","xls"]
 )
 if uploaded_file is None:
     st.info("upload a dataset to continue")
-    st.stop()
+    st.stop()           
 
-df = load_data(uploaded_file)     #data_load funtion kisi bhi file me nahi banana hota hai 
+df = load_data(uploaded_file)
 if df is None:
-    st.error("Unaable to read dataset")   # error detect ke liy use karte hai 
+    st.error("Unable to read dataset")
     st.stop()
 
 working_df = df.copy()
-with st.expander("file Details",expanded=False):
-    details=get_file_details(uploaded_file)
-    st.write(details) # write detail likhne ka kamm karta hai 
+with st.expander("File Details", expanded=False):
+    details= get_file_details(uploaded_file)
+    st.write(details)
 
-    st.header("Dataset Preview")
-    rows = st.slider(
-        "Rows",
-        5,
-        100,
-        10
-    )
-    st.dataframe(
-        working_df.head(rows),
-        use_container_width=True     # space ke liy use karte hai 
-    )
+st.header("Dataset Preview")
+rows = st.slider(
+    "Rows",
+    5,
+    100,
+    10
+)
+st.dataframe(
+    working_df.head(rows),
+    use_container_width=True
+)
 
 st.header("Dataset Overview")
 show_dataset_metrics(working_df)
@@ -73,31 +73,31 @@ c1.metric(
     info["Rows"]
 )
 c2.metric(
-    "columns",
-    info["columns"]
+    "Columns",
+    info["Columns"]
 )
 c3.metric(
     "Memory(MB)",
-    info["Memory usage (MB)"]
+    info["Memory Usage (MB)"]
 )
-c4,c5,c6 = st.coulumn(3)
+c4,c5,c6 = st.columns(3)
 c4.metric(
-    "Missing cells",
-    info["Missing cells"]
+    "Missing Cells",
+    info["Missing Cells"]
 )
 c5.metric(
     "Duplicate Rows",
     info["Duplicate Rows"]
 )
 c6.metric(
-    "Total cells",
-    info["Total cells"]
+    "Total Cells",
+    info["Total Cells"]
 )
-# jab koi specific data show karna hai to as(astype(str)) ka use karte hai 
-st.header("Data types")
+
+st.header("Data Types")
 datatype_df = pd.DataFrame({
-    "column":working_df.columns,
-    "Datatypes":working_df.dtypes.astype(str)
+    "Column":working_df.columns,
+    "Datatype": working_df.dtypes.astype(str)
 })
 
 st.dataframe(
@@ -129,13 +129,13 @@ if not cat_summary.empty:
         use_container_width=True
     )
 else:
-    st.warning("No object datatype columns found")
+    st.warning("No object datatype Columns Found")    
 
 st.header("Column Statistics")
 all_columns = working_df.columns.tolist()
 selected_column = st.selectbox(
     "Select Column",
-    all_columns  
+    all_columns
 )
 
 stats = column_statistics(
@@ -157,181 +157,181 @@ if fig is not None:
         use_container_width=True
     )
 
-#visualization parts 
-st.markdown("----")
-st.header("Data Visulization")
+# visualization part
+st.markdown("---")
+st.header("Data Visualization")
 numeric_cols = numeric_columns(working_df)
 categorical_cols = categorical_columns(working_df)
 plot_type = st.selectbox(
     "Select Visualization",
     [
         "Histogram",
-        "BOx plot",
-        "Density  plot",
-        "violin_plot",
-        "Scatter plot",
+        "Box Plot",
+        "Density Plot",
+        "Violin Plot",
+        "Scatter Plot",
         "Pair plot",
         "Pie Chart",
-        "Count plot",
-        "line_chart",    #time ke corresponding growth show karane ke liy 
-        "Bar chart"
+        "Count Plot",
+        "Line Chart",
+        "Bar Chart"
     ]
 )
-if plot_type =="HIstogram":
+if plot_type =="Histogram":
     column = st.selectbox(
-        "Select Numeric column",
+        "Select Numeric Column",
         numeric_cols,
         key="hist"
-    ) 
+    )
     fig = histogram(working_df,column)
     st.plotly_chart(fig,use_container_width=True)
-elif plot_type =="box_plot":
+elif plot_type == "Box Plot":
     column = st.selectbox(
-         "Select Numeric column",
-        numeric_cols,
-        key="box"           
-       )
+            "Select Numeric Column",
+            numeric_cols,
+            key="box"
+        )
     fig = box_plot(working_df,column)
     st.plotly_chart(fig,use_container_width=True)
-elif plot_type =="Density  plot":
+elif plot_type == "Density Plot":
     column = st.selectbox(
-         "Select Numeric column",
-        numeric_cols,
-        key="Density"           
-       )
+            "Select Numeric Column",
+            numeric_cols,
+            key="density"
+        )
     fig = density_plot(working_df,column)
-    st.plotly_chart(fig,use_container_width=True)
-elif plot_type =="violin_plot":
+    st.plotly_chart(fig,use_container_width=True)  
+elif plot_type == "Violin Plot":
     column = st.selectbox(
-         "Select Numeric column",
-        numeric_cols,
-        key="violin"           
-       )
+            "Select Numeric Column",
+            numeric_cols,
+            key="violin"
+        )
     fig = violin_plot(working_df,column)
-    st.plotly_chart(fig,use_container_width=True)
+    st.plotly_chart(fig,use_container_width=True)       
 elif plot_type =="Scatter Plot":
     col1,col2 = st.columns(2)
-    x =col1.selectbox(
+    x = col1.selectbox(
         "x Axis",
         numeric_cols,
-        key="scatter"
-    )   
-    y =col2.selectbox(
+        key="scatter_x"
+    )
+    y = col2.selectbox(
         "Y Axis",
         numeric_cols,
         key = "scatter_y"
     )
     color = st.selectbox(
         "colour(optional)",
-        [None]+ categorical_cols
+        [None] + categorical_cols
     )
     if color == "None":
-        color = None 
+        color = None
     fig = scatter_plot(
         working_df,
         x,
         y,
         color
-    )
-    st.plotly_chart(fig,use_container_width=True)
+    ) 
+    st.plotly_chart(fig,use_container_width=True)   
 elif plot_type == "Pair plot":
-    st.info("Generating Pair plot..")
+    st.info("Generating Pair plot ..")  
     fig = pair_plot(working_df)
     if fig is not None:
         st.plotly_chart(
             fig,
             use_container_width=True
-        )
+        ) 
     else:
-        st.warning("At leat two numeric columns are required")
+        st.warning("At least two numeric columns are required")     
 elif plot_type == "Pie Chart":
     column = st.selectbox(
         "select categorical column",
         categorical_cols,
-        key="Pie"
+        key="pie"
     )
-    fig = "Pie Chart"(working_df)
-    st.plotly_chart(fig,use_container_width=True)
-elif plot_type == "Count plot":
+    fig = pie_chart(working_df,column) 
+    st.plotly_chart(fig,use_container_width=True)  
+elif plot_type == "Count Plot":
     column = st.selectbox(
         "select categorical column",
         categorical_cols,
-        key="Count"
+        key="count"
     )
-    fig = "Count plot"(working_df,column)
-    st.plotly_chart(fig,use_container_width=True)
-elif plot_type ==  "line_chart":
+    fig = count_plot(working_df,column) 
+    st.plotly_chart(fig,use_container_width=True)   
+elif plot_type == "Line Chart":
     col1,col2 = st.columns(2)
     x = col1.selectbox(
         "X Axis",
         working_df.columns,
-        key=  "line_x"
+        key="line_x"
     )
     y = col2.selectbox(
-            "Y Axis,",
-            working_df.columns,
-            key=  "line_y"
-    )
+        "Y Axis",
+        numeric_cols,
+        key="line_y"
+    ) 
     fig = line_chart(
         working_df,
         x,
-        y,
+        y
+    ) 
+    st.plotly_chart(fig,use_container_width=True) 
+elif plot_type == "Bar Chart":
+    col1,col2 = st.columns(2)
+    x = col1.selectbox(
+        "X Axis",
+        working_df.columns,
+        key="bar_x"
     )
-    st.plotly_chart(fig,use_container_width=True)
-elif plot_type ==   "Bar chart":
-     col1,col2 = st.columns(2)
-     x = col1.selectbox(
-         "X Axis",
-         working_df.columns,
-         key=  "line_x"
-    )
-     y = col2.selectbox(
-             "Y Axis,",
-             working_df.columns,
-             key=  "line_y"
-    )
-     fig = bar_chart(
-         working_df,
-         x,
-         y,
-    )
-st.plotly_chart(fig,use_container_width=True) 
+    y = col2.selectbox(
+        "Y Axis",
+        numeric_cols,
+        key="bar_y"
+    ) 
+    fig = bar_chart(
+        working_df,
+        x,
+        y
+    ) 
+    st.plotly_chart(fig,use_container_width=True) 
 
-#correlation method 
+#correlation
 st.markdown("---")
 st.header("Correlation Analysis")
 method = st.radio(
-    "Correlation method",
+    "Correlation Method",
     [
         "pearson",
         "spearman"
     ],
     horizontal=True
-)
+) 
 corr_fig = correlation_heatmap(
     working_df,
     method
-)
+) 
 if corr_fig is not None:
-    st.plotly_chart(fig,use_container_width=True) 
+    st.plotly_chart(corr_fig,use_container_width=True)          
 
- # outlier analysis
+#outlier analysis
 st.markdown("---")
-st.header("outlier Detection")
+st.header("Outlier Detection")
 column = st.selectbox(
-    "select numberic column",
+    "select numeric column",
     numeric_cols,
     key="outlier"
 )
 fig = outlier_plot(
     working_df,column
 )
-st.plotly_chart(fig,use_container_width=True) 
-#data cleaning 
+st.plotly_chart(fig,use_container_width=True)
+
+#data cleaning
 st.markdown("---")
 st.header("Data Preprocessing")
 preprocessed_df = working_df.copy()
-
 tab1,tab2,tab3,tab4 = st.tabs([
     "Cleaning",
     "Encoding",
@@ -339,37 +339,38 @@ tab1,tab2,tab3,tab4 = st.tabs([
     "Download"
 ])
 with tab1:
-    st.subheader("Data Cleaning ")
+    st.subheader("Data Cleaning")
     cleaning_option = st.selectbox(
         "select cleaning operation",
         [
-            "Fill Missing Value",
-            "Remove Missing Row",
+            "Fill Missing Values",
+            "Remove Missing Rows",
             "Remove Missing Columns",
             "Remove Duplicates",
             "Drop Columns",
             "Rename Column",
-            "Convert Datatypes" 
+            "Convert Datatypes"
         ]
-    ) 
-    if cleaning_option == "Fill Missing Value":
+    )
+    if cleaning_option == "Fill Missing Values":
         column = st.selectbox(
             "Column",
             preprocessed_df.columns
         )
         method = st.selectbox(
             "Method",[
-            "mean",
-            "mode",
-            "constant"
+                "mean",
+                "median",
+                "mode",
+                "constant"
             ]
         )
         value = None
-        if method =="constant":
+        if method == "constant":
             value = st.text_input(
                 "Constant Value"
             )
-        if st.button("Apply FIll Missing"):
+        if st.button("Apply Fill Missing"):
             preprocessed_df = fill_missing(
                 preprocessed_df,
                 column,
@@ -377,35 +378,35 @@ with tab1:
                 value
             )
             st.success("Done")
-            st.dataframe(preprocessed_df.head())
-    elif cleaning_option ==  "Remove Missing Row":
+            st.dataframe(preprocessed_df.head())  
+    elif cleaning_option == "Remove Missing Rows":
         if st.button("Remove"):
             preprocessed_df = remove_missing_rows(preprocessed_df)
             st.success("Rows Removed")
-            st.dataframe(preprocessed_df.head())
+            st.dataframe(preprocessed_df.head()) 
     elif cleaning_option == "Remove Missing Columns":
-         if st.button("Remove"):
+        if st.button("Remove"):
             preprocessed_df = remove_missing_columns(preprocessed_df)
             st.success("Columns Removed")
-            st.dataframe(preprocessed_df.head())
+            st.dataframe(preprocessed_df.head())                 
     elif cleaning_option == "Remove Duplicates":
-         if st.button("Remove Duplicates"):
-            preprocessed_df = remove_missing_columns(preprocessed_df)
-            st.success( "Duplicates Removed ")
-            st.dataframe(preprocessed_df.head())
+        if st.button("Remove Duplicates"):
+            preprocessed_df = remove_duplicates(preprocessed_df) 
+            st.success("Duplicates Removed")
+            st.dataframe(preprocessed_df.head())      
     elif cleaning_option == "Drop Columns":
         cols = st.multiselect(
             "Columns",
             preprocessed_df.columns
-        )
+        ) 
         if st.button("Drop"):
             preprocessed_df = drop_columns(
                 preprocessed_df,
                 cols
             )
-            st.success("Column Droped")
-            st.dataframe(preprocessed_df.head())
-    elif cleaning_option ==  "Rename Column":
+            st.success("Columns Droped")
+            st.dataframe(preprocessed_df.head())      
+    elif cleaning_option == "Rename Column": 
         old = st.selectbox(
             "Old Name",
             preprocessed_df.columns
@@ -420,10 +421,10 @@ with tab1:
                 new
             )
             st.success("Renamed sucessfully")
-            st.dataframe(preprocessed_df.head())
-    elif cleaning_option ==   "Convert Datatypes":
+            st.dataframe(preprocessed_df.head())   
+    elif cleaning_option == "Convert Datatypes":
         col = st.selectbox(
-            "column",
+            "Column",
             preprocessed_df.columns
         )
         dtype = st.selectbox(
@@ -436,30 +437,86 @@ with tab1:
                 "datetime"
             ]
         )
-        if st.button("Conver"):
+        if st.button("Convert"):
             preprocessed_df = convert_datatype(
                 preprocessed_df,
                 col,
                 dtype
+            )   
+            st.success("Datatype Changed")
+            st.dataframe(preprocessed_df.head())         
+with tab2:
+    st.subheader("Encoding")
+    encoding = st.radio(
+        "Encoding Type",
+        [
+            "Label Encoding",
+            "One Hot Encoding"
+        ]
+    )
+    cat_cols = categorical_columns(preprocessed_df)
+    cols = st.multiselect(
+        "Columns",
+        cat_cols
+    )
+    if encoding == "Label Encoding":
+        if st.button("Apply Label Encoding"):
+            preprocessed_df = label_encoder(
+                preprocessed_df,
+                cols
             )
-            st.success("Datatype Change")
+            st.success("Done")
             st.dataframe(preprocessed_df.head())
+    else:
+        if st.button("Apply One Hot Encoding"): 
+            preprocessed_df = one_hot_encode(
+                preprocessed_df,
+                cols
+            ) 
+            st.success("Done")
+            st.dataframe(preprocessed_df.head())   
+with tab3:
+    st.subheader("Scaling")
+    scaling = st.selectbox(
+        "Scaling Method",
+        [
+            "StandardScaler",
+            "MinMaxScaler"
+        ]
+    )
+    num_cols = numeric_columns(preprocessed_df)
+    cols = st.multiselect(
+        "Columns",
+        num_cols
+    )   
+    if st.button("Apply Scaling"):
+        if scaling == "StandardScaler":
+            preprocessed_df = standard_scale(
+                preprocessed_df,
+                cols
+            )
+        else:
+            preprocessed_df = minmax_scaler(
+                            preprocessed_df,
+                            cols
+                        )
+        st.success("Scaling complete")
+        st.dataframe(preprocessed_df.head())
+with tab4:
+    st.subheader("Download processed dataset")
+    csv = convert_csv(preprocessed_df)
+    st.download_button(
+        label = "Download CSV",
+        data = csv,
+        file_name = "processed_dataset.csv",
+        mime = "text/csv"
+    )
 st.markdown("---")
-
 st.markdown(
-    """
-    <div style='text-align:center'>
-    <h4 Smart EDA TOOL</h4>
-    <p>Built with using Streamlit, Plotly & Python</p>
-    </div>
+    """<div style = 'text-align:center'>
+        <h4> Smart Eda Tool</h4>
+        <p> Build by Brajesh Singh</p>
+        </div>
     """,
-    unsafe_allow_html=True
+    unsafe_allow_html = True
 )
-
-
-
-
-
-                                                
-
-
